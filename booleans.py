13bcde1dd9482585e258aab227d8b2f24b99b120
Navinda Chandrasiri <adilanga@gmail.com>
2023-01-15 11:56:17 +0530

    Booleans

---
+++ booleans.py
@@ -0,0 +1,14 @@
+x = "Hello World"
+y = 100
+
+print(bool(x))
+print(bool(y))
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+
