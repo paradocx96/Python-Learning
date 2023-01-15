e21b95e60e207ae2e65c2295390a8e489a8e31bc
Navinda Chandrasiri <adilanga@gmail.com>
2023-01-15 11:55:54 +0530

    Numbers

---
+++ numbers.py
@@ -0,0 +1,47 @@
+# int
+x = 1
+print(type(x))
+
+x = -3255522
+print(type(x))
+
+# complex
+x = 1j
+print(type(x))
+
+x = 3 + 65j
+print(type(x))
+
+x = -587j
+print(type(x))
+
+# float
+x = 2.8
+print(type(x))
+
+x = -25.369
+print(type(x))
+
+x = 35e3
+print(type(x))
+
+x = 12E4
+print(type(x))
+
+x = -87.7e100
+print(type(x))
+
+# random number
+import random
+
+print(random.randrange(1, 10))
+
+# casting
+x = int("5200")
+print(type(x))
+
+x = float("254.25785")
+print(type(x))
+
+x = str(254.3658)
+print(type(x))
