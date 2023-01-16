6a881ae0e874a44b3d664b28d4059f3b5593684b
Navinda Chandrasiri <adilanga@gmail.com>
2023-01-16 20:25:04 +0530

    Math Function

---
+++ math_functions.py
@@ -0,0 +1,19 @@
+import math
+
+x = (25, 36, 14, 95, 21)
+y = (14, 85, 24, 61, 43)
+
+print(min(x))
+print(max(x))
+
+print(abs(-7.25))
+
+print(pow(2, 10))
+
+print(math.sqrt(100))
+
+print(math.ceil(7.25))  # round up -> 8
+print(math.floor(7.25))  # round down -> 7
+
+print(math.pi)
+print(math.e)
